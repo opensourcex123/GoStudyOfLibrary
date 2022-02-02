85f92422d8b5bad4cea03f83659d3e55f3944b93
nfx.git <13561592791@163.com>
2022-02-02 19:00:47 +0800

    viper库

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.16
 require (
 	github.com/go-ini/ini v1.66.3 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/spf13/viper v1.10.1 // indirect
 )
---
+++ viper.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"github.com/spf13/viper"
+	"log"
+)
+
+// go每日一库之viper库。viper 是一个配置解决方案，拥有丰富的特性
+//	设置文件名时不要带后缀；
+//	搜索路径可以设置多个，viper 会根据设置顺序依次查找；
+//	viper 获取值时使用section.key的形式，即传入嵌套的键名；
+//	默认值可以调用viper.SetDefault设置。
+func main() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("toml")
+	viper.AddConfigPath(".")
+	viper.SetDefault("redis.port", 6381)
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatal("read config failed", err)
+	}
+
+	fmt.Println(viper.Get("app_name"))
+	fmt.Println(viper.Get("log_level"))
+
+	fmt.Println("mysql ip: ", viper.Get("mysql.ip"))
+	fmt.Println("mysql port: ", viper.Get("mysql.port"))
+	fmt.Println("mysql user: ", viper.Get("mysql.user"))
+}
